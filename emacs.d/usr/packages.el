a2df9d0cd9d7499c83636d97e6c39c6328a274bf
Samuel Schulter <samuel@ma21-nb2.local>
2021-03-18 23:16:52 -0400

    Update melpa package URL

--- emacs.d/usr/packages.el
+++ emacs.d/usr/packages.el
@@ -5,7 +5,7 @@
 
 (setq package-archives
       '(("gnu" . "http://elpa.gnu.org/packages/")
-        ("melpa" . "http://melpa.milkbox.net/packages/")
+        ("melpa" . "http://melpa.org/packages/")
         ("org" . "http://orgmode.org/elpa/")
         ("elpy" . "http://jorgenschaefer.github.io/packages/")))
 (package-initialize)
